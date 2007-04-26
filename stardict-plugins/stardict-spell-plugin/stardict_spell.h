78dcd0d7940de0f4804497d958e1f332f7b77066
huzheng <huzheng@localhost>
2007-04-26 10:17:18 +0000

    add spell plugin

---
+++ stardict-plugins/stardict-spell-plugin/stardict_spell.h
@@ -0,0 +1,13 @@
+#ifndef _STARDICT_SPELL_PLUGIN_H_
+#define _STARDICT_SPELL_PLUGIN_H_
+
+#include "../../src/lib/plugin.h"
+#include "../../src/lib/virtualdictplugin.h"
+
+extern "C" {
+	extern bool stardict_plugin_init(StarDictPlugInObject *obj);
+	extern void stardict_plugin_exit(void);
+	extern bool stardict_virtualdict_plugin_init(StarDictVirtualDictPlugInObject *obj);
+}
+
+#endif
