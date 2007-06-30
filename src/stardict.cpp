f758309a939c9b15e5ab7041781f605ab799b79f
huzheng <huzheng@localhost>
2007-06-30 01:38:11 +0000

    upgrade to GPL3

--- src/stardict.cpp
+++ src/stardict.cpp
@@ -1,10 +1,10 @@
 /* StarDict - A international dictionary for GNOME.
  * http://stardict.sourceforge.net
- * Copyright (C) 2003-2006 HuZheng <huzheng_001@163.com>
+ * Copyright (C) 2003-2007 Hu Zheng <huzheng_001@163.com>
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
+ *  the Free Software Foundation; either version 3 of the License, or
  *  (at your option) any later version.
  *
  *  This program is distributed in the hope that it will be useful,
@@ -13,8 +13,7 @@
  *  GNU Library General Public License for more details.
  *
  *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 /*
