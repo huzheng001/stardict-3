01f588b60beea8f66c8562eaabd5003bb51fa720
huzheng <huzheng@localhost>
2007-10-15 10:16:20 +0000

    add some python tools

--- stardict-plugins/stardict-wordnet-plugin/stardict_wordnet.cpp
+++ stardict-plugins/stardict-wordnet-plugin/stardict_wordnet.cpp
@@ -83,7 +83,7 @@ DLLIMPORT bool stardict_plugin_init(StarDictPlugInObject *obj)
 		return true;
 	}
 	obj->type = StarDictPlugInType_SPECIALDICT;
-	obj->info_xml = g_strdup_printf("<plugin_info><name>%s</name><version>1.0</version><short_desc>%s</short_desc><long_desc>%s</long_desc><author>Hu Zheng &lt;huzheng_001@163.com&gt;</author><website>http://stardict.sourceforge.net</website></plugin_info>", _("WordNet dict rendering"), _("WordNet dict rendering engine."), _("Render the WordNet dictionary."));
+	obj->info_xml = g_strdup_printf("<plugin_info><name>%s</name><version>1.0</version><short_desc>%s</short_desc><long_desc>%s</long_desc><author>Hu Zheng &lt;huzheng_001@163.com&gt;</author><website>http://stardict.sourceforge.net</website></plugin_info>", _("WordNet dict rendering"), _("WordNet dict rendering engine."), _("Render the WordNet dictionary.\nStatement: The engine of this plugin comes from dedict(http://sevenpie.net), which is developed by Bian Peng &lt;tianpmoon@gmail.com&gt;, much thanks for his open source sharing!"));
 	obj->configure_func = configure;
 	plugin_info = obj->plugin_info;
 	plugin_service = obj->plugin_service;
---
+++ stardict-tools/src/mkguangyunst.py
@@ -0,0 +1,20 @@
+import sys, string
+for line in sys.stdin.readlines():
+	words = string.split(line[:-1], '\t')
+	muci  = words[1]
+	sheng = words[2]
+	deng  = words[3]
+	hu    = words[4]
+	yunbu = words[5]
+	diao  = words[6]
+	fanqie= words[7]
+	she   = words[8]
+	chars = words[9]
+	romazi= words[10]
+	beizhu= words[12]
+	pinyin= words[13]
+	psyun = words[22]
+	if beizhu == '':
+		print "%s\t%s %s%s%s%s%s%s %sQIE PINYIN%s PSYUN%s\\n%s" % (romazi, muci, sheng, yunbu, she, hu, deng, diao, fanqie, pinyin, psyun, chars)
+	else:
+		print "%s\t%s %s%s%s%s%s%s %sQIE PINYIN%s PSYUN%s\\n%s\\n%s" % (romazi, muci, sheng, yunbu, she, hu, deng, diao, fanqie, pinyin, psyun, chars, beizhu)
