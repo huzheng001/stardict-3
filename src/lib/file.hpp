e5988c002775c2e8f8bbdaf018eafa37447940dd
kubtek <kubtek@localhost>
2010-03-07 10:54:45 +0000

    changed dictionary search algorithm, rules tightened

--- src/lib/file.hpp
+++ src/lib/file.hpp
@@ -14,10 +14,10 @@ void __for_each_file(const std::string& dirname, const std::string& suff,
 			 const List& order_list, const List& disable_list, 
 			 Function f)
 {
-	GDir *dir = g_dir_open(dirname.c_str(), 0, NULL);	
+	GDir *dir = g_dir_open(dirname.c_str(), 0, NULL);
 	if (dir) {
-		const gchar *filename;		
-		while ((filename = g_dir_read_name(dir))!=NULL) {	
+		const gchar *filename;
+		while ((filename = g_dir_read_name(dir))!=NULL) {
 			std::string fullfilename(dirname+G_DIR_SEPARATOR_S+filename);
 			if (g_file_test(fullfilename.c_str(), G_FILE_TEST_IS_DIR))
 				__for_each_file(fullfilename, suff, order_list, disable_list, f);
@@ -46,6 +46,59 @@ void for_each_file(const List& dirs_list, const std::string& suff,
 	}
 	for (it=dirs_list.begin(); it!=dirs_list.end(); ++it)
 		__for_each_file(*it, suff, order_list, disable_list, f);
+}
+
+template<typename Function>
+void __for_each_file_restricted(const std::string& dirname, const std::string& suff,
+			 const List& order_list, const List& disable_list, 
+			 Function f)
+{
+	GDir *dir = g_dir_open(dirname.c_str(), 0, NULL);
+	if(!dir)
+		return;
+	const gchar *filename;
+	bool found = false;
+	// search for a file with a suff suffix
+	while ((filename = g_dir_read_name(dir))!=NULL) {
+		std::string fullfilename(dirname+G_DIR_SEPARATOR_S+filename);
+		if (!g_file_test(fullfilename.c_str(), G_FILE_TEST_IS_DIR)
+			&& g_str_has_suffix(filename, suff.c_str())) {
+			found = true;
+			if(std::find(order_list.begin(), order_list.end(), fullfilename)==order_list.end()) { 
+				bool disable=std::find(disable_list.begin(), disable_list.end(), fullfilename)!=disable_list.end();
+				f(fullfilename, disable);
+			}
+		}
+	}
+	g_dir_close(dir);
+	if(found)
+		return;
+	// descend into subdirectories
+	dir = g_dir_open(dirname.c_str(), 0, NULL);
+	if(!dir)
+		return;
+	while ((filename = g_dir_read_name(dir))!=NULL) {
+		std::string fullfilename(dirname+G_DIR_SEPARATOR_S+filename);
+		if (g_file_test(fullfilename.c_str(), G_FILE_TEST_IS_DIR))
+			__for_each_file_restricted(fullfilename, suff, order_list, disable_list, f);
+	}
+	g_dir_close(dir);
+}
+
+/* like for_each_file, but does not descend into subdirectories of 
+ * a directory where a file with suff suffix is found. */
+template<typename Function>
+void for_each_file_restricted(const List& dirs_list, const std::string& suff,
+			 const List& order_list, const List& disable_list, 
+			 Function f)
+{
+	List::const_iterator it;
+	for (it=order_list.begin(); it!=order_list.end(); ++it) {
+		bool disable=std::find(disable_list.begin(), disable_list.end(), *it)!=disable_list.end();
+		f(*it, disable);
+	}
+	for (it=dirs_list.begin(); it!=dirs_list.end(); ++it)
+		__for_each_file_restricted(*it, suff, order_list, disable_list, f);
 }
 
 template<typename Function>
