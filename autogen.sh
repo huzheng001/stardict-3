8ff8bae424ec1f603f908085faeddb28670de283
kubtek <kubtek@localhost>
2010-03-26 12:12:54 +0000

    Migrating to GNOME Documentation Build Utilities

--- autogen.sh
+++ autogen.sh
@@ -1,50 +1,23 @@
 #!/bin/sh
+# Run this to generate all the initial makefiles, etc.
 
-(intltoolize --version) < /dev/null > /dev/null 2>&1 || {
-	echo;
-	echo "You must have intltool installed to compile Stardict";
-	echo;
-	exit;
+srcdir=`dirname $0`
+test -z "$srcdir" && srcdir=.
+
+PKG_NAME="stardict"
+REQUIRED_AUTOMAKE_VERSION=1.9
+
+(test -f $srcdir/configure.in \
+  && test -f $srcdir/ChangeLog \
+  && test -d $srcdir/src) || {
+    echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
+    echo " top-level stardict directory"
+    exit 1
 }
 
-(libtoolize --version) < /dev/null > /dev/null 2>&1 || {
-	echo;
-	echo "You must have libtool installed to compile Stardict";
-	echo;
-	exit;
+
+which gnome-autogen.sh || {
+    echo "You need to install gnome-common package"
+    exit 1
 }
-
-(automake --version) < /dev/null > /dev/null 2>&1 || {
-	echo;
-	echo "You must have automake installed to compile Stardict";
-	echo;
-	exit;
-}
-
-(autoconf --version) < /dev/null > /dev/null 2>&1 || {
-	echo;
-	echo "You must have autoconf installed to compile Stardict";
-	echo;
-	exit;
-}
-
-echo "Generating configuration files for Stardict, please wait...."
-echo;
-
-echo "Running intltoolize ...."
-intltoolize --force --copy || exit;
-
-echo "Running libtoolize, please ignore non-fatal messages...."
-echo n | libtoolize --copy --force || exit;
-
-echo "Running aclocal...."
-aclocal -I m4 || exit;
-echo "Running autoheader...."
-autoheader || exit;
-echo "Running automake --add-missing --copy...."
-automake --add-missing --copy;
-echo "Running autoconf ...."
-autoconf || exit;
-echo "Running automake ...."
-automake || exit;
-./configure --prefix=/usr --sysconfdir=/etc --mandir=/usr/share/man --disable-deprecations $@
+USE_GNOME2_MACROS=1 . gnome-autogen.sh "$@"
