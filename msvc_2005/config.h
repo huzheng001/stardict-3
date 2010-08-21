7b18a99777497650e32856e06ac6178bc058853e
kubtek <kubtek@localhost>
2010-08-21 13:22:40 +0000

    g_stat function declaration differ between versions of glib update code that typedefs stardict_stat_t - second parameter of g_stat this solution works for Windows and Unix

--- msvc_2005/config.h
+++ msvc_2005/config.h
@@ -1,11 +1,9 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-#include <glib.h>
-#include <glib/gstdio.h>
-
 #if defined(_MSC_VER)
-# include <stdlib.h>
+#  include <glib.h>
+#  include <glib/gstdio.h>
 
 #  define S_IRWXU 0
 #  define __attribute__(x) /**/
@@ -20,18 +18,6 @@ G_END_DECLS
 
 #endif
 
-/* g_stat function is declared differently depending on the version of glib 
-GLIB_MICRO_VERSION version number may be incorrect, adjust it if needed. 
-Use stardict_stat_t in the source code to define a structure for g_stat. */
-#if !GLIB_CHECK_VERSION(2, 20, 0)
-// int g_stat (const gchar *filename, struct stat *buf);
-typedef struct stat stardict_stat_t;
-#elif !GLIB_CHECK_VERSION(2, 25, 0)
-// int g_stat (const gchar *filename, struct _g_stat_struct *buf);
-typedef struct _g_stat_struct stardict_stat_t;
-#else
-// int g_stat (const gchar *filename, GStatBuf *buf);
-typedef GStatBuf stardict_stat_t;
-#endif
+#include <../config-custom.h>
 
 #endif/*!CONFIG_H*/
