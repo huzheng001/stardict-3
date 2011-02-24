60963a4f9c54c3d0932578f19a12c34778b91dcd
kubtek <kubtek@localhost>
2011-02-24 18:40:57 +0000

    Remove empty configure function in plugins and assign NULL to function pointers. Setting function pointer to NULL allows Mange plugins dialog to disable Configure plugin button when the plugin does provide settings to be configured.

--- stardict/stardict-plugins/stardict-wiki-parsedata-plugin/stardict_wiki_parsedata.cpp
+++ stardict/stardict-plugins/stardict-wiki-parsedata-plugin/stardict_wiki_parsedata.cpp
@@ -26,10 +26,6 @@ static bool parse(const char *p, unsigned int *parsed_size, ParseResult &result,
 	return true;
 }
 
-static void configure()
-{
-}
-
 DLLIMPORT bool stardict_plugin_init(StarDictPlugInObject *obj, IAppDirs* appDirs)
 {
 	if (strcmp(obj->version_str, PLUGIN_SYSTEM_VERSION)!=0) {
@@ -38,7 +34,7 @@ DLLIMPORT bool stardict_plugin_init(StarDictPlugInObject *obj, IAppDirs* appDirs
 	}
 	obj->type = StarDictPlugInType_PARSEDATA;
 	obj->info_xml = g_strdup_printf("<plugin_info><name>%s</name><version>1.0</version><short_desc>%s</short_desc><long_desc>%s</long_desc><author>Hu Zheng &lt;huzheng001@gmail.com&gt;</author><website>http://stardict.sourceforge.net</website></plugin_info>", _("Wiki data parsing"), _("Wiki data parsing engine."), _("Parse the wiki data."));
-	obj->configure_func = configure;
+	obj->configure_func = NULL;
 	return false;
 }
 
