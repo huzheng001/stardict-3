e1e9f42b5bd7bd2a466e5a5b106c33df843e9f6d
kubtek <kubtek@localhost>
2011-03-06 06:00:30 +0000

    change Docklet class for Unix to use GtkStatusIcon GTK+ class

--- stardict/src/docklet.h
+++ stardict/src/docklet.h
@@ -3,35 +3,34 @@
 
 #include <gtk/gtk.h>
 #include "tray.hpp"
-#include "eggtrayicon.h"
-#include "lib/utils.h"
 
-class AppSkin;//forward declaration
+class AppSkin;
 
 class DockLet : public TrayBase {
 public:
-	DockLet(GtkWidget *, bool, const AppSkin&);
-	~DockLet();
+	DockLet(GtkWidget *mainwin, bool is_scan_on, const AppSkin& skin);
+	virtual ~DockLet();
 	void minimize_to_tray();
-	void set_scan_mode(bool);
 private:
-	EggTrayIcon *docklet_;
-	GtkWidget *image_; //icon image.
-	typedef  ResourceWrapper<GtkWidget, GtkWidget, void, gtk_widget_destroy> GMenu;
-	GMenu menu_;
-	GtkWidget *scan_menuitem_;
-	bool embedded_;
 	GdkPixbuf *normal_icon_, *stop_icon_, *scan_icon_;
+	GtkStatusIcon *docklet_;
+	GtkWidget *menu_;
+	GtkWidget *scan_menuitem_;
 
-	static void on_embedded(GtkWidget *widget, gpointer data);
-	static void on_destroyed(GtkWidget *widget, DockLet *oDockLet);
-	static gboolean on_btn_press(GtkWidget *, GdkEventButton *, DockLet *);
+	void create_docklet(void);
+	void create_popup_menu(void);
+
 	static void on_menu_scan(GtkCheckMenuItem *, gpointer);
-	static void on_menu_quit(GtkMenuItem *, gpointer);	
-	static gboolean on_docklet_create(gpointer data);
+	static void on_menu_quit(GtkMenuItem *, gpointer);
+	static void on_popup_menu(GtkStatusIcon *status_icon,
+		guint          button,
+		guint          activate_time,
+		gpointer       user_data);
+	static gboolean on_button_press(GtkStatusIcon *status_icon,
+		GdkEventButton      *event,
+		gpointer       user_data);
 
-	void popup_menu(GdkEventButton *event);
-	void create_docklet();
+protected:
 	void scan_on();
 	void scan_off();
 	void show_normal_icon();
