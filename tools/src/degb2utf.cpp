64c2e392ef265bfe4d0dee69f3721a95bcaa1745
kubtek <kubtek@gmail.com>
2011-07-03 10:09:12 +0400

    add copying permission statement to all files without any copying permission statement at all

--- tools/src/degb2utf.cpp
+++ tools/src/degb2utf.cpp
@@ -1,3 +1,20 @@
+/*
+ * This file is part of StarDict.
+ *
+ * StarDict is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * StarDict is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with StarDict.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 // Some Germany-Chinese dictionary data files are encoded both in Germany and Chinese, that is, Windows-1252 and GBK, this program try to convert these data files to UTF-8 format.
 
 #include "stdio.h"
