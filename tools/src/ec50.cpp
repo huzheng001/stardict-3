d0092b22750f30d2fd54ba848498556a9cfa5800
kubtek <kubtek@gmail.com>
2011-07-03 10:09:12 +0400

    add copying permission statement to all files without any copying permission statement at all

--- tools/src/ec50.cpp
+++ tools/src/ec50.cpp
@@ -1,3 +1,20 @@
+/*
+ * This file is part of StarDict.
+ *
+ * StarDict is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * StarDict is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with StarDict.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 //file format crack by Luo XiaoHui <tipyluo@hotmail.com>, this program is written by Hu Zheng <huzheng_001@163.com>
 //you can mail us for the demo program which have more comment.
 //encoding of this file is Chinese simplified -> GB18030
