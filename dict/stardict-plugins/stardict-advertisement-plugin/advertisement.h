0911494a89d96b9dda54631067fd784f8b606495
huzheng001 <huzheng001@gmail.com>
2014-05-15 19:28:09 +0800

    Stardict-advertisement-plugin comes back!

---
+++ dict/stardict-plugins/stardict-advertisement-plugin/advertisement.h
@@ -0,0 +1,23 @@
+#ifndef _STARDICT_ADVERTISEMENT_PLUGIN_H_
+#define _STARDICT_ADVERTISEMENT_PLUGIN_H_
+
+#ifdef _WIN32
+#if BUILDING_DLL
+# define DLLIMPORT __declspec (dllexport)
+#else /* Not BUILDING_DLL */
+# define DLLIMPORT __declspec (dllimport)
+#endif /* Not BUILDING_DLL */
+#else
+# define DLLIMPORT
+#endif
+
+#include "../../src/lib/plugin.h"
+#include "../../src/lib/virtualdictplugin.h"
+
+extern "C" {
+	DLLIMPORT extern bool stardict_plugin_init(StarDictPlugInObject *obj);
+	DLLIMPORT extern void stardict_plugin_exit(void);
+	DLLIMPORT extern bool stardict_virtualdict_plugin_init(StarDictVirtualDictPlugInObject *obj);
+}
+
+#endif
